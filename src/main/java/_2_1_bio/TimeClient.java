7487a19577d9ac40b58ed43bbc3769871f47808b
JBearded <244239255@qq.com>
2015-09-04 17:17:49 +0800

    t

--- src/main/java/_2_1_bio/TimeClient.java
+++ src/main/java/_2_1_bio/TimeClient.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 public class TimeClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
 		int port = 8080;
 		if(args != null && args.length > 0){
